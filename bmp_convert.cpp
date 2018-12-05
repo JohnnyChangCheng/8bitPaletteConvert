e64e68a3b55ed57840bd37050c76958416617a26
johnny <johnny.chang@atgames.net>
2018-12-05 14:39:16 +0800

    segementfault

--- bmp_convert.cpp
+++ bmp_convert.cpp
@@ -43,7 +43,6 @@ class Image_t{
         ~Image_t(){
             delete bmp_header;
             delete bmp_info;
-            delete[] palette;
         };
         int Parser(std::string filename){     
             std::fstream fs;
@@ -51,6 +50,7 @@ class Image_t{
             fs.read((char*)bmp_header,sizeof(BITMAPFILEHEADER_t));
             fs.read((char*)bmp_info,sizeof(BITMAPINFOHEADER_t)); 
             palette_size = bmp_info->biClrUsed;
+            std::cout << "palette : "<<palette_size << std::endl;
             palette = new pixel_t[palette_size];
             fs.read((char*)palette,sizeof(pixel_t)*bmp_info->biClrUsed); 
             length_image = bmp_header->bfSize - bmp_header->bfOffBits;
@@ -60,19 +60,20 @@ class Image_t{
             return 0;
         }
         uint32_t length_image;
-        uint8_t palette_size;
+        uint32_t palette_size;
         BITMAPFILEHEADER_t *bmp_header;
         BITMAPINFOHEADER_t *bmp_info;
         pixel_t* palette;
         uint8_t* container = NULL;
 };
 int* Palette_compare(Image_t* src){
-    int* mapper = new int[256]{0};
+    int* mapper = new int[src->palette_size]{0};
+    std::cout << "old palette_size "<< src->palette_size<< std::endl;
     for(int i = 0;i < src->palette_size; ++i)
 	{
 		int min_index = 0;
 		int min_dist = INT_MAX;
-		for(int j = 0 ; j < 128; ++j)
+		for(int j = 0 ; j < COLOR_NUM; ++j)
 		{
 
 			int r = src->palette[i].r - Palette[j].r;
@@ -84,31 +85,41 @@ int* Palette_compare(Image_t* src){
 			if(dist < min_dist )
 			{
 				min_dist = dist;
-				min_index = j<<1;
+				min_index = j;
 			}
 		}
 		mapper[i] = min_index;
 	}
     return mapper;
 }
-void SetPalette(Image_t *src,int *mapper){
-    pixel_t* palette= new pixel_t[256];
-    memset(palette,0,sizeof(pixel_t)*256);
-    for(int i =0 ;i<128;i++){
-        palette[i<<1].r = Palette[i].r;
-        palette[i<<1].g = Palette[i].g;
-        palette[i<<1].b = Palette[i].b;
+void SetPalette(Image_t &src,int *mapper){
+    pixel_t* new_palette= new pixel_t[COLOR_NUM];
+    memset(new_palette,0,sizeof(pixel_t)*COLOR_NUM);
+    for(int i =0 ;i<COLOR_NUM;i++){
+        new_palette[i].r = Palette[i].r;
+        new_palette[i].g = Palette[i].g;
+        new_palette[i].b = Palette[i].b;
     }
-    for(int i =0 ;i <src->length_image;i++ ){
-        src->container[i]= mapper[src->container[i]];
+    for(int i =0 ;i <src.length_image;i++ ){
+        src.container[i]= mapper[src.container[i]];
     }
-     src->palette = palette;
-     int diff = 256-src->bmp_info->biClrUsed;
-     src->bmp_header->bfOffBits += diff*4;
-     src->bmp_info->biSizeImage += diff*4;
-     src->bmp_header->bfSize += diff*4;
-     src->bmp_info->biClrUsed = 256;
-
+     src.palette = new_palette;
+     int diff = COLOR_NUM-src.bmp_info->biClrUsed;
+     if(diff >= 0){
+        src.bmp_header->bfOffBits += diff*4;
+        src.bmp_info->biSizeImage += diff*4;
+        src.bmp_header->bfSize += diff*4;
+        src.bmp_info->biClrUsed = COLOR_NUM;
+        src.bmp_info->biClrImportant = 0;
+     }
+     else{
+         diff = diff*-1;
+        src.bmp_header->bfOffBits -= diff*4;
+        src.bmp_info->biSizeImage -= diff*4;
+        src.bmp_header->bfSize -= diff*4;
+        src.bmp_info->biClrUsed = COLOR_NUM;
+        src.bmp_info->biClrImportant = 0;
+     }
 }
 int main(int argc ,char* argv[]){
     Image_t src_image;
@@ -116,10 +127,12 @@ int main(int argc ,char* argv[]){
     std::string outfilename(argv[2]);
     src_image.Parser(filename);
     std::cout << "BMP Format bit : "<<src_image.bmp_info->biBitCount<<std::endl;
+    std::cout << "BMP offset : "<<src_image.bmp_header->bfOffBits<<std::endl;
     std::cout << "BMP FILE SIZE : "<<src_image.bmp_header->bfSize<<std::endl;
     std::cout << "PALETTE SIZE : "<<src_image.bmp_info->biClrUsed<<std::endl;
+    std::cout << "Image SIZE : "<<src_image.bmp_info->biSizeImage<<std::endl;
     int* mapper =  Palette_compare(&src_image);
-    SetPalette(&src_image,mapper);
+    SetPalette(src_image,mapper);
  
  
     std::fstream of;
@@ -129,5 +142,5 @@ int main(int argc ,char* argv[]){
     of.write((char*)src_image.palette,sizeof(pixel_t)*src_image.bmp_info->biClrUsed);
     of.write((char*)src_image.container,src_image.length_image);
     of.close();
-
+    return 0;
 }
